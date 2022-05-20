64d2dcf74e74655f4190482a264d21aaefa15fb0
hoangitVKU <thhoang.20it3@vku.udn.vn>
2022-05-20 20:02:35 +0700

    dev
    laij bug ddos

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,41 @@ function onChange() {
     const $ = document.querySelector.bind(document);
 
     const teamNumber = $('#team-number')
-    const member = $('#member-list')
-
+    const member_list_1 = $('#member-list_1')
+    const member_list_2 = $('#member-list_2')
     var html = [];
+    if (teamNumber.value == 1) {
+
+        html = '<div class="each-member">'
+        html += ' <div class="member-title"> Thành viên 1 </div>'
+        html += '<div class="member-form">'
+        html += ' <div class="form-group">'
+        html += '<label class="" for="">Họ và tên </label>'
+        html += ' <input type="text" name="fullName_1" class="" value="" id="" required placeholder="Nguyễn Văn A">'
+        html += '</div>'
+        html += '<div class="form-group">'
+        html += '<label class="" for="">Email </label>'
+        html += ' <input type="email" name="email_1" class="" value="" id="" required placeholder="example@gmail.com">'
+        html += '</div>'
+        html += ' <div class="form-group">'
+        html += '<label class="" for="">Số điện thoại </label>'
+        html += ' <input type="number" name="phoneNumber_1" class="" value="" id="" required placeholder="09xxxxxxxx">'
+        html += '</div>'
+        html += ' <div class="form-group">'
+        html += '<label class="" for="">Kĩ năng </label>'
+        html += ' <input type="text" name="skill_1" class="" value="" id="" required placeholder="Laravel, NodeJs,...">'
+        html += '</div>'
+        html += '<div class="form-group">'
+        html += ' <label class="" for="">Số tài khoản </label>'
+        html += '<input type="number" name="bankNumber" class="" value="" id="" placeholder="Không bắt buộc">'
+        html += '</div>'
+        html += '</div>'
+        html += '</div>'
+        member_list_1.innerHTML = html
+        member_list_2.innerHTML = ''
+    }
     if (teamNumber.value > 1) {
+
         for (var i = 2; i <= teamNumber.value; i++) {
             html[i] = '<div class="each-member">'
             html[i] += ' <div class="member-title"> Thành viên ' + i + ' </div>'
@@ -29,8 +60,8 @@ function onChange() {
             html[i] += '</div>'
             html[i] += '</div>'
         }
-        member.innerHTML = html
-    } else {
-        console.log('<=1')
+        console.log(html)
+
+        member_list_2.innerHTML = html
     }
 }
